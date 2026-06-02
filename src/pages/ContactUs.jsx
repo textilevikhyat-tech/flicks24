501bda0e05bc468f922b885a0835dbcbd76460f4
textilevikhyat-tech <textilevikhyat@gmail.com>
2026-06-02 13:20:56 +0530

    Create ContactUs.jsx

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,8 @@
+const ContactUs = () => (
+  <div className="p-4 max-w-2xl mx-auto">
+    <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
+    <p>Email: textilevikhyat@gmail.com</p>
+    <p>Support: flicks24vikhyatg@gmail.com</p>
+  </div>
+)
+export default ContactUs
